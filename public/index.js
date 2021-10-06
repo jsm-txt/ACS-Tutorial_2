be5808442e5021e655f950fa15ad59d43b58a6ed
Jose <hose153@gmail.com>
2021-10-05 22:53:46 -0500

    user can leave chats

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 
 $(document).ready(()=>{
   const socket = io.connect();
-  
   let user;
+  // Get the online users from the server
+  socket.emit('get online users');
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
@@ -46,4 +47,20 @@ $(document).ready(()=>{
     </div>
   `);
   })
+
+  socket.on('get online users', (onlineUsers) => {
+    //You may have not have seen this for loop before. It's syntax is for(key in obj)
+    //Our usernames are keys in the object of onlineUsers.
+    for (username in onlineUsers) {
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    }
+  })
+
+  //Refresh the online user list
+  socket.on('user has left', (onlineUsers) => {
+    $('.users-online').empty();
+    for (username in onlineUsers) {
+      $('.users-online').append(`<p>${username}</p>`);
+    }
+  });
 })
